645ae4c42728022a0a510be8244cde3636ad3508
MuhammetIncirkus <164883670+MuhammetIncirkus@users.noreply.github.com>
2024-11-25 10:37:59 +0100

    Initial Commit

---
+++ MindJourney_Projektwoche2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindJourney_Projektwoche2
+//
+//  Created by Muhammet Incirkus on 25.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindJourney_Projektwoche2/MindJourney_Projektwoche2App.swift
@@ -0,0 +1,17 @@
+//
+//  MindJourney_Projektwoche2App.swift
+//  MindJourney_Projektwoche2
+//
+//  Created by Muhammet Incirkus on 25.11.24.
+//
+
+import SwiftUI
+
+@main
+struct MindJourney_Projektwoche2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
