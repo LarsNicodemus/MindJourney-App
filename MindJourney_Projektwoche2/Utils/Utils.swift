48b0518dd4e0932d53a1021b0a5303f984bf608d
andreasbeer <b.beer.dev@gmail.com>
2024-11-27 17:25:36 +0100

    Return hex string to Color

--- MindJourney_Projektwoche2/Utils/Utils.swift
+++ MindJourney_Projektwoche2/Utils/Utils.swift
@@ -19,3 +19,19 @@ func colorToHexString(_ color: UIColor) -> String {
         Int(green * 255),
         Int(blue * 255))
 }
+
+func hexStringToColor(_ hex: String) -> UIColor? {
+    var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+    hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+    
+    var rgb: UInt64 = 0
+    guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else {
+        return nil
+    }
+    
+    let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+    let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+    let blue = CGFloat(rgb & 0x0000FF) / 255.0
+    
+    return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+}
